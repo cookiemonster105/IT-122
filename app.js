3fcfecf0baa597b23a7e663f22247c07cf69f302
cookiemonster105 <journee@gmail.com>
2021-07-18 15:53:37 -0700

    week 2 commit

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 import http from 'http';
-import fs from 'fs';
+
+// import function from data.js
+import { getAll, getItem } from './data.js';
+
+import { parse } from "querystring";
 
 http.createServer(
    (req, res) => {
@@ -7,20 +11,33 @@ http.createServer(
     
 var path = req.url.toLowerCase();
 
-switch(path){
-    case '/':
-        fs.readFile('home.html', (err, data) => {
-            if (err) return console.error(err);
-            res.writeHead(200, {'Content-Type': 'text/html'});
-            res.end (data.toString());
+  let url = req.url.split("?");  // separate route from query string
+  let query = parse(url[1]); // convert query string to a JS object
+  console.log(query);
+  console.log(query.name);
 
-        })
+switch(url[0]){
+    case '/':
+
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.end (JSON.stringify( getAll()));
+
         break;
 
     case '/about':
         res.writeHead(200, {'Content-Type': 'text/plain'});
         res.end ('About Page');
+        res.end (JSON.stringify(getAll, getItem));
         break;
+
+    case '/detail':
+        res.writeHead(200, {'Content-Type': 'text/plain'});
+        res.end (JSON.stringify(getItem(query.name)));
+
+
+
+        break;
+    
 
     default:
         res.writeHead(404, {'Content-Type': 'text/plain'});
@@ -32,7 +49,9 @@ switch(path){
   }
 ).listen(process.env.PORT || 3000);
 
-/////////////////// 
+
+
+
 
 
 
--- data.js
+++ data.js
@@ -1,12 +1,48 @@
 let bookcase = [
-{name : Billy, type : bookcase, height : 63, width : 79, price : 98, color : white },
+{name : "Billy", type : "bookcase", height : 63, width : 79, price : 98, color : "white" },
 {name : "Kallax", type : "shelf unit", height : 30, width : 57, price : 69.99, color : "white"},
 {name : "Laiva", type : "bookcase", height : 24, width : 65, price : 19.99, color : "black"},
 {name : "Eket", type : "Storage combination with feet", height : 27, width : 13, price : 62, color : "white"},
 {name : "Hauga", type : "High cabinet with 2 doors", height : 27, width : 78, price : 298, color : "birch veneer/glass"},
 {name : "Oxberg", type : "Bookcase with doors", height : 31, width : 79, price : 109, color : "black"},
-{name : "Havsta", type : "Shelving unit with base", height : 24, width : 83, price : 169, color : "gray"},
-]
+{name : "Havsta", type : "Shelving unit with base", height : 24, width : 83, price : 169, color : "gray"}
+];
+// // Returns # of bookcases 
+// console.log(bookcase.length)
 
-console.log(bookcase.length)
-for (i=0; i<students.length;i++){console.log(bookcase[i])}
+// Returns all bookcases 
+
+    // //console.log(getAll);
+    // const getAll = bookcase.map((name)(bookcaseAll) => {
+    //     return bookcase.map(bookcase);
+    //     };
+    // console.log(getAll);
+
+
+const getAll = () => {
+         return bookcase;
+ };
+
+
+
+
+
+// Returns single bookcase
+const getItem = (bookcaseName) => {
+    return bookcase.find((name) =>{
+        return name.name.toLowerCase() == bookcaseName.toLowerCase();
+    });
+}
+
+
+
+// //Function to return bookcases that cost more than $72.00
+// let filter = bookcase.filter((bookcase) => {
+//     return bookcase.price > 72
+// });
+
+//  console.log(filter);
+
+ //exports functions
+export{getAll, getItem};
+ 
