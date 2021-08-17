0dde639339abf485bf92ff8b94f88bc9764993ba
cookiemonster105 <journee@gmail.com>
2021-08-16 18:18:10 -0700

    fix update

--- test_db.js
+++ test_db.js
@@ -7,19 +7,6 @@ Bookcase.find({}).lean()
   })
   .catch(err => next(err));
 
-// // return all records that match a condition
-// Bookcase.find({"author": "Smith" }).lean()
-//   .then((Bookcase) => {
-//     console.log(bookcase);
-//   })
-//   .catch(err => next(err));
-
-// // return a single record
-// Bookcase.findOne({"title": "Dune" }).lean()
-//   .then((boocaasek) => {
-//       console.log(bookcase);;
-//   })
-//   .catch(err => next(err));
 
 // insert or update a single record
 // const newBookcase = {'name':'Finnby', 'type':'bookcase', 'height': '24', 'width' : '70' , 'price' : '30' , 'color' : 'black'  }
